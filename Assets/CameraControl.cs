255991b4c0178da299ad728b1ebd950dda70b6e4
Ben Tristem <github@bentristem.com>
2016-04-24 11:20:40 +0100

    03 User Interface » File Management 2.screenflow

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public int tiltAngle;
+    int rollAngle;
+    private int pitchAngle;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
