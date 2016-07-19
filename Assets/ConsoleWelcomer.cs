c6cf8650a61a617afe4c3262b1214bcdd1e7f6b9
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-19 19:24:31 +0100

    02 Unity Interface- Creating Scripts

---
+++ Assets/ConsoleWelcomer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsoleWelcomer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hello my name is Sam.");
+        print("Welcome to my program.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
