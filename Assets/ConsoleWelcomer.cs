ac9155f3200971c96837dcecb1c13c6ad3f729ef
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-20 20:10:11 +0100

    04 MethodsFunctions » Declaration & Use

--- Assets/ConsoleWelcomer.cs
+++ Assets/ConsoleWelcomer.cs
@@ -7,13 +7,23 @@ public class ConsoleWelcomer : MonoBehaviour {
 	void Start () {
         int anInteger = 1000;
         float aFloat = 2.5f;
-        float sum = anInteger + aFloat;
-        float average = sum / 2;
+        float aSecondFloat = 10.0f;
         print("anInteger = " + anInteger);
         print("aFloat = " + aFloat);
-        print("sum = " + sum);
-        print("average = " + average);
+        print("average = " + Average(aFloat, aSecondFloat));
+        print("square aFloat = " + Square(Square(aFloat)));
 	}
+
+    float Square (float x)
+    {
+        return x * x;
+    }
+
+    float Average (float a, float b)
+    {
+        float sum = a + b;
+        return sum / 2;
+    }
 	
 	// Update is called once per frame
 	void Update () {
