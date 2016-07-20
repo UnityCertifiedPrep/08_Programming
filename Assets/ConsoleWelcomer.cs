e84d9c2e5d2276dc0a8deaaaf22274c2b3666b7f
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-20 16:00:19 +0100

    03 Variables » Integer, Floating Point

--- Assets/ConsoleWelcomer.cs
+++ Assets/ConsoleWelcomer.cs
@@ -5,8 +5,14 @@ public class ConsoleWelcomer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        print("Hello my name is Sam.");
-        print("Welcome to my program.");
+        int anInteger = 1000;
+        float aFloat = 2.5f;
+        float sum = anInteger + aFloat;
+        float average = sum / 2;
+        print("anInteger = " + anInteger);
+        print("aFloat = " + aFloat);
+        print("sum = " + sum);
+        print("average = " + average);
 	}
 	
 	// Update is called once per frame
