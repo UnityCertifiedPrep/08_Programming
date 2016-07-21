6dba4637636389e4758241c9b14b862dc3cf04ce
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-21 13:15:03 +0100

    06 Variables » Vector3 (Objects and Classes)

--- Assets/ConsolePrinter.cs
+++ Assets/ConsolePrinter.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class ConsolePrinter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        print("Hello world!");
+    // Use this for initialization
+    void Start() {
+        Vector3 currentLocation = new Vector3(1.0f, 4.0f, 3.0f);
+        Vector3 home = new Vector3(2.0f, 4.0f, 5.0f);
+        Vector3 locationToHome = currentLocation - home;
+        print("Distance to home " + locationToHome.magnitude);
 	}
 	
 	// Update is called once per frame
