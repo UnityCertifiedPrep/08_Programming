42e2bd9e31d7d4bb76fc942a93f38230d219ddac
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-21 17:28:24 +0100

    08 Time » DeltaTime

--- Assets/ProjectileMovement.cs
+++ Assets/ProjectileMovement.cs
@@ -3,21 +3,16 @@ using System.Collections;
 
 public class ProjectileMovement : MonoBehaviour {
 
-    public Vector3 newPosition;
+    public float speed = 5;
 
 	// Use this for initialization
 	void Start () {
 
 	}
 
-    public void UpdatePosition()
-    {
-        Transform trans = GetComponent<Transform>();
-        trans.position = newPosition;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
+    // Update is called once per frame
+    void Update() {
+        Vector3 positionDelta = new Vector3(0, 0, speed * Time.deltaTime);
+        transform.position = transform.position + positionDelta;
 	}
 }
