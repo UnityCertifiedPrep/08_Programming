54224b26233d86ebd2e592ab8afee6785a75fce3
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-21 16:54:14 +0100

    07 GameObject » Components

---
+++ Assets/MovementTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ProjectileMovement projectileMovementObject = GetComponent<ProjectileMovement>();
+        projectileMovementObject.UpdatePosition();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/ProjectileMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMovement : MonoBehaviour {
+
+    public Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    public void UpdatePosition()
+    {
+        Transform trans = GetComponent<Transform>();
+        trans.position = newPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
