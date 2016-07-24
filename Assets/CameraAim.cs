6d8cb3f0a70343c025d023dc5f47e33a28f4b54c
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-24 13:11:34 +0100

    11 Camera API » ScreenPointToRay

---
+++ Assets/CameraAim.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera ourCamera = GetComponent<Camera>();
+        Ray aimRay = ourCamera.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(aimRay.origin, aimRay.direction, Color.red);
+    }
+}
