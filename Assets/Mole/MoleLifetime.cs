34bbb3e51fde948d0dae2c954ec4aa23ee8a961f
Samuel Pattuzzi <sam@pattuzzi.it>
2016-07-25 19:14:22 +0100

    13 Improving Gameplay

---
+++ Assets/Mole/MoleLifetime.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoleLifetime : MonoBehaviour
+{
+    public float duration = 5;
+    float startTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTime + duration < Time.time)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Score!");
+        Destroy(collision.collider.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Mole/RandomSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpawner : MonoBehaviour {
+
+    public float interval = 5;
+    public GameObject prefab;
+    public float spawnRadius = 5;
+    float lastSpawnTime = 0;
+    
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (lastSpawnTime + interval < Time.time)
+        {
+            var randomOffset = Random.insideUnitSphere * spawnRadius;
+            Instantiate(prefab, transform.position + randomOffset, transform.rotation);
+            lastSpawnTime = Time.time;
+        }
+	}
+}
